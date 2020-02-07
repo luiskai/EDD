84de609b3a05a624b3fd30c34fa523eef02ba54f
Luis Nares Signes <luis_ador_01@hotmail.com>
2020-02-07 20:30:31 +0100

    Prova

--- Unitat 4. Automatització amb make/Java/ant/projecte/com/ieseljust/edd/calc/Calcula.java
+++ Unitat 4. Automatització amb make/Java/ant/projecte/com/ieseljust/edd/calc/Calcula.java
@@ -8,7 +8,7 @@ public class Calcula {
 
     public static void main(String[] args) {
         if (args.length != 2) {
-            System.out.println("\nSintaxi incorrecta. Necessite dos números.");
+            System.out.println("\nSintaxi incorrecta. Necessite dos numeros.");
             System.exit(-1);
         }
 
